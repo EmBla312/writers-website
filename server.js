eb0e107082ca803473f000bd45971ed22a5c1870
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2020-06-02 21:45:01 -0500

    db connected & working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.urlencoded());
+//app.use(express.urlencoded());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -15,15 +15,39 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log('Server is starting on PORT, ', PORT);
+    console.log('MONGODB_URI: ', process.env.MONGODB_URI);
+    add_user();
+
 })
 /****************************************** */
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGOLAB_URI; //environment variable storing your connection url - hidden for security purposes
+const uri = process.env.MONGODB_URI; //environment variable storing your connection url - hidden for security purposes
+const dbname = 'writeweb-db';
 
-MongoClient.connect(uri, (err, db) => {
-    if(!err) console.log("Connection Success");
-});
+function add_user() {
+    MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+        if (err) console.log('ERROR MONGODB: ', err, client);
+        else {
+            var db = client.db(dbname);  //get database object
+
+            var users_collection = db.collection('users');  //get colleciton in database
+
+            var userdoc = {
+                username: 'larry',
+                password: 'abcde1$',
+                email: 'larry@gmail.com',
+                createDateTime: { month: 'June', day: 2, year: 2020, hour: 12, minute: 39, second: 57 },
+                lastlogin: { month: 'June', day: 1, year: 2020, hour: 13, minute: 0, second: 4 }
+            }
+
+            users_collection.insert(userdoc);
+        }
+
+
+    });
+}
+
 
 
